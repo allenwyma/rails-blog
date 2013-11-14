737b287cf274e4d418b9d112ce298090126b55e6
Muzyka Lukasz <muzyka.lukasz@gmail.com>
2013-11-14 14:42:21 +0800

    some changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :verify_as_admin, except: [:index, :show]
+  #before_action :verify_as_admin, except: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
